cf8f83e61d54de6c09d340e2e1e911098b1a4c0b
Abuu001 <abulugz@gmail.com>
2021-02-24 19:54:14 +0300

    compares words in two strings

---
+++ anagrams.js
@@ -0,0 +1,38 @@
+/**
+ * checks if 2 string are anagrams of each other
+ * one string is an anagram of another if it uses exact same character
+ * 
+ * eg
+ * anagrams('heart', 'earth')  -->True
+ *  anagrams('heart', '       earth')  -->True
+ *  anagrams('Heart', 'EARTH')  -->True
+ *  anagrams('lol', 'loic')  -->False
+ */
+
+const anagrams = (str1 , str2)=> {
+    str1 = str1.toLowerCase().replace(/[\W_]+/g,"");
+    str2 = str2.toLowerCase().replace(/[\W_]+/g,"")
+
+    if (str1.length  !== str2.length) {
+        return false;
+    }
+
+    const str1CharCount = {};
+    for (let i=0; i<str1.length ; i++) {
+        const aChar = str1[i]
+        str1CharCount[aChar] = str1CharCount[aChar] + 1 || 1;
+    }
+ 
+    for (let i=0; i<str2.length ; i++) {
+        const bChar = str2[i]
+        if ( !str1CharCount[bChar]){
+            return false
+        }else{
+            str1CharCount[bChar]-- ;
+        }
+    }
+    return true
+}
+
+console.log(anagrams('heart', 'earth'));
+console.log(anagrams('Heart', 'EARTH'));
